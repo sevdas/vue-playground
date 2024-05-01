82b09eee8d675b80a1f54cdef28ad53cc93f58a6
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2024-05-01 17:04:44 +0100

    create button component with event listener and emit event handler

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { Slot } from 'vue'
+
+defineProps<{ children: Slot; onClick: () => void; required: true }>()
+defineEmits(['onClick'])
+</script>
+
+<template>
+  <button @onClick="$emit('onClick')">{{ children }}</button>
+</template>
+
+<style scoped lang="scss"></style>
